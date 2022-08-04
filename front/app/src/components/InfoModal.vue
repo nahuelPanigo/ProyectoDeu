78e98708e0ea9495394682d6dab0eccacf7af846
Nahuel Panigo <nahpanigo99@gmail.com>
2022-08-04 18:51:39 -0300

    modal

---
+++ front/app/src/components/InfoModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal fade" tabindex="-1" role="dialog" id="myModalInfo">   
+    <div class="modal-dialog" role="document">
+    <div class="modal-content back">
+    <h1>ZONAS DE RIESGO</h1>
+    <h2>Elegir tema del sitio:</h2>
+    <h3>Zona Muy Alta: color bordo oscuro</h3>
+    <h3>Zona Alta: color rojo</h3>
+    <h3>Zona media: color amarillo</h3>
+    <h3>Zona baja: color gris</h3>
+    <h3>Zona sin riesgo detectado: sin color</h3>
+        <div id="botones">
+            <button type="button" data-dismiss="modal">Cerrar</button>
+        </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name : "InfoPopUp",  
+}
+</script>
--- front/app/src/views/MapaZonaRiesgos.vue
+++ front/app/src/views/MapaZonaRiesgos.vue
@@ -2,17 +2,23 @@
   <div class="Centros De Evacuacion">
     <h1>Mapa Zonas de riesgo</h1>
     <h2> En este mapa se visualizan las distintas zonas de la ciudad, distinguiendose los niveles de riesgo.</h2>
+    <button  class="btn" data-toggle="modal" data-target="#myModalInfo"> 
+     <i class="bi bi-gear-fill" id ="config-icon">Info Zonas</i>   
+    </button>
+    <InfoPopUp/>
     <RiskMap></RiskMap>
   </div>
 </template>
 
 <script>
 import RiskMap from '../components/RiskMap.vue'
+import InfoPopUp  from '../components/InfoModal.vue'
 
 export default {
   name: 'RiskMapView',
   components: {
-    RiskMap
+    RiskMap,
+    InfoPopUp
   }
 }
 </script>
